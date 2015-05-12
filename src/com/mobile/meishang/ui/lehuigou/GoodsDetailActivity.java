21e3e139736c6391903209e02f5363cc19219a12
小A <466455592@163.com>
2015-05-12 10:18:50 +0800

    商品详情

--- src/com/mobile/meishang/ui/lehuigou/GoodsDetailActivity.java
+++ src/com/mobile/meishang/ui/lehuigou/GoodsDetailActivity.java
@@ -3,7 +3,6 @@ package com.mobile.meishang.ui.lehuigou;
 import java.util.List;
 
 import android.graphics.Paint;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
